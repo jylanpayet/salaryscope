6a9b981c5b484593413fec31d4763dc58a795309
Assem92 <34968174+Assem92@users.noreply.github.com>
2024-01-28 13:55:57 +0100

    Add files via upload

---
+++ controller.py
@@ -0,0 +1,26 @@
+from flask import Flask, jsonify, request
+import json
+import plotly.utils
+from dataVisualization import Visualization  
+
+app = Flask(__name__)
+vis = Visualization()
+
+@app.route("/visualization")
+def visualization():
+    heuresParSemaineBySalary = json.dumps(vis.heuresParSemaineBySalary(), cls=plotly.utils.PlotlyJSONEncoder)
+    repartitionSalaryInTheWorld = json.dumps(vis.repartitionSalaryInTheWorld(), cls=plotly.utils.PlotlyJSONEncoder)
+    trancheSalary = json.dumps(vis.trancheSalarialParRace(), cls=plotly.utils.PlotlyJSONEncoder)
+    positiveCapitalByWorkclass = json.dumps(vis.positive_capital_by_workclass(), cls=plotly.utils.PlotlyJSONEncoder)
+    positiveCapitalByEducation = json.dumps(vis.positive_capital_by_education(), cls=plotly.utils.PlotlyJSONEncoder)
+    test = json.dumps(vis.test(), cls=plotly.utils.PlotlyJSONEncoder)
+    return jsonify({"repartitionSalaryInTheWorld": repartitionSalaryInTheWorld,
+                    "trancheSalary":trancheSalary,
+                    "positiveCapitalByWorkclass":positiveCapitalByWorkclass,
+                    "heuresParSemaineBySalary":heuresParSemaineBySalary,
+                    "positiveCapitalByEducation":positiveCapitalByEducation
+                    })
+
+if __name__ == "__main__":
+    app.run(debug=True)
+    
